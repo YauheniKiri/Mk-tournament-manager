cd6e432fbd653d407d9643bfba5ef6a17ac72a6c
Yauheni Kirychenka <yauhenikirychenka@gmail.com>
2024-12-27 21:24:10 +0300

    Add data

---
+++ src/data/characters.ts
@@ -0,0 +1,177 @@
+export const characters = [
+  {
+    id: 1,
+    name: "Нуб сайбот",
+    image: "/assets/images/characters/noob-saibot.png",
+  },
+  {
+    id: 2,
+    name: "Сайракс",
+    image: "/assets/images/characters/cyrax.png",
+  },
+  {
+    id: 3,
+    name: "Ермак",
+    image: "/assets/images/characters/ermac.png",
+  },
+  {
+    id: 4,
+    name: "Китана",
+    image: "/assets/images/characters/kitana.png",
+  },
+  {
+    id: 5,
+    name: "Милина",
+    image: "/assets/images/characters/mileena.png",
+  },
+  {
+    id: 6,
+    name: "Таня",
+    image: "/assets/images/characters/tanya.png",
+  },
+  {
+    id: 7,
+    name: "Рейн",
+    image: "/assets/images/characters/rain.png",
+  },
+  {
+    id: 8,
+    name: "Смоук",
+    image: "/assets/images/characters/smoke.png",
+  },
+  {
+    id: 9,
+    name: "Скорпион",
+    image: "/assets/images/characters/scorpion.png",
+  },
+  {
+    id: 10,
+    name: "Рептилия",
+    image: "/assets/images/characters/reptile.png",
+  },
+  {
+    id: 11,
+    name: "Саб-зиро",
+    image: "/assets/images/characters/sub-zero.png",
+  },
+  {
+    id: 12,
+    name: "Ли мей",
+    image: "/assets/images/characters/limei.png",
+  },
+  {
+    id: 13,
+    name: "Кенши",
+    image: "/assets/images/characters/kenshi.png",
+  },
+  {
+    id: 14,
+    name: "Барака",
+    image: "/assets/images/characters/baraka.png",
+  },
+  {
+    id: 15,
+    name: "Герас",
+    image: "/assets/images/characters/geras.png",
+  },
+  {
+    id: 16,
+    name: "Омни-мэн",
+    image: "/assets/images/characters/omni-man.png",
+  },
+  {
+    id: 17,
+    name: "Миротворец",
+    image: "/assets/images/characters/peacemaker.png",
+  },
+  {
+    id: 18,
+    name: "Гоуст фейс",
+    image: "/assets/images/characters/ghostface.png",
+  },
+  {
+    id: 19,
+    name: "Т-1000",
+    image: "/assets/images/characters/t-1000.png",
+  },
+  {
+    id: 20,
+    name: "Сектор",
+    image: "/assets/images/characters/sektor.png",
+  },
+  {
+    id: 21,
+    name: "Такеда",
+    image: "/assets/images/characters/takeda.png",
+  },
+  {
+    id: 22,
+    name: "Шан цзун",
+    image: "/assets/images/characters/shang-tsung.png",
+  },
+  {
+    id: 23,
+    name: "Генерал Шао",
+    image: "/assets/images/characters/general-shao.png",
+  },
+  {
+    id: 24,
+    name: "Синдел",
+    image: "/assets/images/characters/sindel.png",
+  },
+  {
+    id: 25,
+    name: "Рейко",
+    image: "/assets/images/characters/reiko.png",
+  },
+  {
+    id: 26,
+    name: "Рейден",
+    image: "/assets/images/characters/raiden.png",
+  },
+  {
+    id: 27,
+    name: "Лю Кан",
+    image: "/assets/images/characters/liu-kang.png",
+  },
+  {
+    id: 28,
+    name: "Джонни Кейдж",
+    image: "/assets/images/characters/johnny.png",
+  },
+  {
+    id: 29,
+    name: "Кун Лао",
+    image: "/assets/images/characters/kung-lao.png",
+  },
+  {
+    id: 30,
+    name: "Ашра",
+    image: "/assets/images/characters/ashrah.png",
+  },
+  {
+    id: 31,
+    name: "Нитара",
+    image: "/assets/images/characters/nitara.png",
+  },
+  {
+    id: 32,
+    name: "Хавик",
+    image: "/assets/images/characters/havik.png",
+  },
+  {
+    id: 33,
+    name: "Куан чи",
+    image: "/assets/images/characters/quan-chi.png",
+  },
+  {
+    id: 34,
+    name: "Хоумлендер",
+    image: "/assets/images/characters/homelander.png",
+  },
+  {
+    id: 35,
+    name: "Конан-варвар",
+    image: "/assets/images/characters/conan.png",
+  },
+];
